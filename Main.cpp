66e344dcbb35808f6a5e00c050c2d0acb2fd2ed4
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-08 16:11:03 +0100

    WINDOWS FTP CLIENT WITH FUNCTIONS

--- Main.cpp
+++ Main.cpp
@@ -22,28 +22,29 @@
 
 using namespace std;
 
+int errorCheckWSAStartup(int check);						//Make sure WSAStartup worked correctly
+struct sockaddr_in createSocket(int port, char ip[]);		//Creates a new socket
+void errorCheckConnect(int check);							//Make sure connect worked correctly
+void sendUserInput(char buffer[], int sd);					//Let user type in command line and send data to socket
+void receive(char buffer[], int bufferSize, int sd);		//Receive data from socket
+
 #define SERV_PORT 21
 #define MAX_MSG 100
 
 int main(int argc, char *argv[])  {
 
+	//Startup for winsock
 	WORD wVersionRequested = 0x0101;
 	WSADATA wsaData;
 	int err;
 
-	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0) {
-		/* Tell the user that we could not find a usable */
-		/* Winsock DLL.                                  */
-		printf("WSAStartup failed with error: %d\n", err);
-		return 1;
-	}
+	err = WSAStartup(wVersionRequested, &wsaData); 
+	errorCheckWSAStartup(err); //Check if WSAStartup worked correctly
 
-	int sd, rc, n;
-	struct sockaddr_in /*localAddr,*/ servaddr;
-	//struct hostent *h;
-	char buffer[4096];
-	char buffer1[4096]; //buffersend
+
+	int sd, rc, port = SERV_PORT;
+	struct sockaddr_in servaddr;
+	char buffer[4096];	//buffer for sending and receiving data
 
 	//Create a socket for the client
 	//If sd<0 there was an error in the creation of the socket
@@ -51,33 +52,64 @@ int main(int argc, char *argv[])  {
 	{
 		perror("Problem in creating the socket"); //exit(2);
 	}
-
-	//Creation of the socket
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("130.226.195.126"); // her skulle der stå argv[1] ved ipadressen dog erstattet vi den med ip addressen til den server vi skal kommunikere med
-	// servaddr.sin_port = htons(portno); //convert to big-endian order
-	servaddr.sin_port = htons(SERV_PORT); //convert to big-endian order
+	
+	servaddr = createSocket(port, "130.226.195.126"); //Initialize socket
 
 	//connect server method
 	rc = connect(sd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	if (rc < 0){
+	errorCheckConnect(rc);
+
+	while (1){
+		receive(buffer, sizeof(buffer), sd); //Recieve data from socket
+		sendUserInput(buffer, sd); //Let user type in the command line and send the data to the socket
+
+	}
+
+}
+
+int errorCheckWSAStartup(int check){
+	if (check != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", check);
+		return 1;
+	}
+	return 0;
+}
+
+struct sockaddr_in createSocket(int port, char ip[]){
+	struct sockaddr_in servaddr;
+	//Creation of the socket
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip); // her skulle der stÃ¥ argv[1] ved ipadressen dog erstattet vi den med ip addressen til den server vi skal kommunikere med
+	// servaddr.sin_port = htons(portno); //convert to big-endian order
+	servaddr.sin_port = htons(port); //convert to big-endian order
+	return servaddr;
+}
+
+void errorCheckConnect(int check){
+	if (check < 0){
 		perror("cannot connect");
 		exit(1);
 	}
+}
 
-	//code for receivind statement ""
-	while (1){
-		n = recv(sd, buffer, sizeof(buffer), 0);
-		buffer[n] = '\0';
-		printf("%s", buffer);
-
-		if (fgets(buffer1, 60, stdin) != NULL) {
-			/* writing content to stdout */
-			puts(buffer1);
-		}
-		strcat(buffer1, "\r\n");
-		send(sd, buffer1, strlen(buffer1), 0);
+void sendUserInput(char buffer[], int sd){
+	//sending code
+	if (fgets(buffer, 60, stdin) != NULL) {
+		/* writing content to stdout */
+		//puts(buffer1);
 	}
+	strcat(buffer, "\r\n");
+	send(sd, buffer, strlen(buffer), 0);
+}
 
+
+void receive(char buffer[], int bufferSize, int sd){
+	//code for receivind statement ""
+	int n;
+	n = recv(sd, buffer, bufferSize, 0);
+	buffer[n] = '\0';
+	printf("%s", buffer);
 }
